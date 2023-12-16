e2aa9497e31a4644587f1380d8d29f833bc6ee87
JohnFrank <Frank_Ibarra400@hotmail.com>
2023-12-16 12:40:53 -0600

    update

--- Flask/src/hello.py
+++ Flask/src/hello.py
@@ -1,17 +1,15 @@
 from flask import Flask, render_template
-'''
- Tutorial: https://www.youtube.com/watch?v=icbGQxVJB7I&list=LL&index=6
- min: 22:00
-'''
+
 app = Flask(__name__)
 
-@app.route("/<name>")
-def hello(name):
-    return f"<h1>Hola, {name}. </h1>"
+@app.route("/")
+def hello():
+    tareas = ["sqlalchemy", "flask", "tkinter"]
+    return render_template('index.html', tarea = tareas)
 
 @app.route("/Diana/")
 def diana():
     return f"<h1>Te amo</h1>"
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
